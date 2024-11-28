32fe122659545f71dc37a0ba536c5d0ae7423b95
Jakub Michna <jmichna03@gmail.com>
2024-11-29 00:59:35 +0100

    Test

--- initial/src/main/java/com/example/restservice/RestServiceApplication.java
+++ initial/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
+		if (args.length > 0 && "--version".equals(args[0])) {
+            System.out.println("Version: 0.0.1-SNAPSHOT");
+            return;
+        }
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
