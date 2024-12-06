aecdb70bba0f096cd0f5e2af0b6800cf7b1f7820
Jakub Michna <jmichna03@gmail.com>
2024-12-06 12:55:51 +0100

    Test

--- initial/src/main/java/com/example/restservice/UserRepository.java
+++ initial/src/main/java/com/example/restservice/UserRepository.java
@@ -2,11 +2,9 @@ package com.example.restservice;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.restservice.User;
-
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+    // No additional methods required for basic CRUD operations
 }
--- initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
+++ initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -1,14 +1,14 @@
 package com.example.restservice;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // Boots the entire Spring context for testing
 public class RestServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        // This test will pass if the application context loads successfully
+    }
 
 }
